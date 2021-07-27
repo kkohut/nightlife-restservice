268436ddd74b00c95e8daab3329e9a640c623c06
Kevin Kohut <kevin.kohut@student.fhws.de>
2021-07-27 19:36:37 +0200

    Initial commit

---
+++ src/main/java/de/nightlife/restservice/NightlifeRestserviceApplication.java
@@ -0,0 +1,13 @@
+package de.nightlife.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightlifeRestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightlifeRestserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/nightlife/restservice/NightlifeRestserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.nightlife.restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightlifeRestserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
