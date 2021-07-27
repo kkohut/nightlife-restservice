1b147105a595e96bb4eb364616e60ce0098518ce
Kevin Kohut <kevin.kohut@student.fhws.de>
2021-07-27 19:47:19 +0200

    Add Artist model

---
+++ src/main/java/de/nightlife/restservice/models/Artist.java
@@ -0,0 +1,42 @@
+package de.nightlife.restservice.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Artist
+{
+	private @Id @GeneratedValue Long id;
+	private String name;
+
+	public Artist( )
+	{
+	}
+
+	public Artist( Long id, String name )
+	{
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId( )
+	{
+		return id;
+	}
+
+	public void setId( Long id )
+	{
+		this.id = id;
+	}
+
+	public String getName( )
+	{
+		return name;
+	}
+
+	public void setName( String name )
+	{
+		this.name = name;
+	}
+}
