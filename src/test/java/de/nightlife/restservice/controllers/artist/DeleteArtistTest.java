4cd8b76e1782107678534f3dd7cdc1a9ebb2b19c
Kevin Kohut <kevin.kohut@student.fhws.de>
2021-08-15 23:55:39 +0200

    Add test case for deleting nonexistent id, add mocking of deleteById method

--- src/test/java/de/nightlife/restservice/controllers/artist/DeleteArtistTest.java
+++ src/test/java/de/nightlife/restservice/controllers/artist/DeleteArtistTest.java
@@ -1,15 +1,19 @@
 package de.nightlife.restservice.controllers.artist;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.nightlife.restservice.models.Artist;
 import de.nightlife.restservice.repositories.ArtistRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.mockito.ArgumentMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,6 +31,8 @@ public class DeleteArtistTest {
     public void delete_artist_existing_returns_nothing_204() throws Exception {
         final Long id = 3L;
 
+        Mockito.doNothing().when(artistRepository).deleteById(id);
+
         mockMvc.perform(MockMvcRequestBuilders
                         .delete("/artists/" + id)
                         .contentType(MediaType.APPLICATION_JSON))
@@ -37,5 +43,17 @@ public class DeleteArtistTest {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("$.name").doesNotExist());
+    }
+
+    @Test
+    public void delete_artist_nonexisting_returns_nothing_404() throws Exception {
+        final Long id = Long.MAX_VALUE;
+
+        Mockito.doThrow(new EmptyResultDataAccessException(1)).when(artistRepository).deleteById(id);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/artists/" + id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
     }
 }
